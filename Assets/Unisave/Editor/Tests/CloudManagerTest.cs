d4c57ecea831f09716c6742d6f769989d2e95c70
Jirka-Mayer <jirkamayer97@gmail.com>
2019-04-03 11:44:08 +0200

    More cloud manager tests

--- Assets/Unisave/Editor/Tests/CloudManagerTest.cs
+++ Assets/Unisave/Editor/Tests/CloudManagerTest.cs
@@ -22,6 +22,9 @@ public class CloudManagerTest
 		public ServerApi.SaveResult saveResult;
 		public ServerApi.LogoutResult logoutResult;
 
+		public Action<string, JsonObject> onSave;
+		public Action<string, JsonObject> onLogout;
+
 		public IEnumerator Login(Action<ServerApi.LoginResult> callback, string email, string password)
 		{
 			callback.Invoke(loginResult);
@@ -30,12 +33,14 @@ public class CloudManagerTest
 
 		public IEnumerator Save(Action<ServerApi.SaveResult> callback, string accessToken, JsonObject playerData)
 		{
+			onSave.Invoke(accessToken, playerData);
 			callback.Invoke(saveResult);
 			yield break;
 		}
 
 		public IEnumerator Logout(Action<ServerApi.LogoutResult> callback, string accessToken, JsonObject playerData)
 		{
+			onLogout.Invoke(accessToken, playerData);
 			callback.Invoke(logoutResult);
 			yield break;
 		}
@@ -142,44 +147,206 @@ public class CloudManagerTest
 		Assert.IsFalse(manager.Login(null, "email", "password"));
 	}
 
-	/*[Test]
-	public void ItCanLoadDistributeCollectAndSave()
+	[Test]
+	public void LoginSuccessCallbackGetsCalled()
 	{
-		PlayerPrefs.SetString(CloudManager.LOCAL_DEBUG_PLAYER_PREFS_KEY, new JsonObject()
-			.Add("foo", "lorem ipsum")
-			.ToString()
+		api.loginResult = new ServerApi.LoginResult {
+			type = ServerApi.LoginResultType.OK,
+			accessToken = "<access-token>",
+			playerData = new JsonObject()
+		};
+
+		bool successCalled = false;
+		bool failureCalled = false;
+		
+		manager.Login(
+			() => { successCalled = true; },
+			failure => { failureCalled = true; },
+		"email", "password");
+
+		Assert.True(successCalled);
+		Assert.False(failureCalled);
+	}
+
+	[Test]
+	public void LoginFailureCallbackGetsCalled()
+	{
+		api.loginResult = new ServerApi.LoginResult {
+			type = ServerApi.LoginResultType.InvalidCredentials
+		};
+
+		bool successCalled = false;
+		bool failureCalled = false;
+		
+		manager.Login(
+			() => { successCalled = true; },
+			failure => {
+				failureCalled = true;
+				Assert.AreEqual(LoginFailure.FailureType.BadCredentials, failure.type);
+			},
+		"email", "password");
+
+		Assert.False(successCalled);
+		Assert.True(failureCalled);
+	}
+
+	[Test]
+	public void LocalDebugPlayerCanLogin()
+	{
+		PlayerPrefs.SetString(
+			CloudManager.LocalDebugPlayerPrefsKey,
+			new JsonObject()
+				.Add("foo", "hello world")
+				.Add("bar", 42)
+				.ToString()
 		);
 		PlayerPrefs.Save();
 
-		UnisavePreferences preferences = ScriptableObject.CreateInstance<UnisavePreferences>();
-		CloudManager manager = new CloudManager(preferences);
-		manager.Login(null, preferences.localDebugPlayerEmail, "secret");
-
-		GameObject go = new GameObject("FakeGameObject");
-		FakeBehaviour behaviour = go.AddComponent<FakeBehaviour>();
-
-		Assert.IsNull(behaviour.foo);
+		manager.Login(null, "local", "");
 
 		manager.Load(behaviour);
 
-		Assert.AreEqual("lorem ipsum", behaviour.foo);
+		Assert.AreEqual("hello world", behaviour.foo);
+		Assert.AreEqual(42, (int)repo.Get("bar"));
+	}
 
-		behaviour.foo = "changed value";
+	//////////
+	// Save //
+	//////////
+	
+	[Test]
+	public void SavingCollectsDataAndSendsThemToServer()
+	{
+		api.loginResult = new ServerApi.LoginResult {
+			type = ServerApi.LoginResultType.OK,
+			accessToken = "<access-token>",
+			playerData = new JsonObject().Add("foo", "initial-value")
+		};
+
+		manager.Login(null, "email", "password");
+		manager.Load(behaviour);
+
+		behaviour.foo = "changed-value";
+
+		bool saveApiCalled = false;
+
+		api.saveResult = new ServerApi.SaveResult {
+			type = ServerApi.SaveResultType.OK
+		};
+
+		api.onSave = (accessToken, playerData) => {
+			saveApiCalled = true;
+			Assert.AreEqual("<access-token>", accessToken);
+			Assert.AreEqual(DataRepositoryHelper.ToJsonObject(repo).ToString(), playerData.ToString());
+		};
 
 		manager.Save();
 
-		JsonObject o = JsonReader.Parse(
-			PlayerPrefs.GetString(CloudManager.LOCAL_DEBUG_PLAYER_PREFS_KEY, "{}"
-		)).AsJsonObject;
-		Assert.AreEqual("changed value", (string)o["foo"]);
+		Assert.True(saveApiCalled);
+	}
 
-		behaviour.foo = "changed again";
+	[Test]
+	public void LocalDebugPlayerCanBeSaved()
+	{
+		PlayerPrefs.DeleteKey(CloudManager.LocalDebugPlayerPrefsKey);
+		PlayerPrefs.Save();
+
+		manager.Login(null, "local", "");
+		manager.Load(behaviour);
+
+		behaviour.foo = "new-value";
+
+		api.onSave = (_, __) => Assert.Fail("Save api method was called when shouldn't.");
+
+		manager.Save();
+
+		var json = LightJson.Serialization.JsonReader.Parse(
+			PlayerPrefs.GetString(CloudManager.LocalDebugPlayerPrefsKey, "{}")
+		);
+
+		Assert.AreEqual("new-value", (string)json["foo"]);
+		Assert.AreEqual(1, json.AsJsonObject.Count);
+	}
+
+	[Test]
+	public void UnexpectedLogoutOnSavingTriggersLogout()
+	{
+		api.loginResult = new ServerApi.LoginResult {
+			type = ServerApi.LoginResultType.OK,
+			accessToken = "<access-token>",
+			playerData = new JsonObject()
+		};
+
+		manager.Login(null, "email", "password");
+
+		api.saveResult = new ServerApi.SaveResult {
+			type = ServerApi.SaveResultType.NotLoggedIn
+		};
+		api.onSave = (_, __) => {};
+
+		Assert.True(manager.LoggedIn);
+
+		manager.Save();
+
+		Assert.IsNull(manager.AccessToken);
+		Assert.False(manager.LoggedIn);
+	}
+
+	////////////
+	// Logout //
+	////////////
+
+	[Test]
+	public void LogoutCollectsDataAndSendsThemToServer()
+	{
+		api.loginResult = new ServerApi.LoginResult {
+			type = ServerApi.LoginResultType.OK,
+			accessToken = "<access-token>",
+			playerData = new JsonObject().Add("foo", "initial-value")
+		};
+
+		manager.Login(null, "email", "password");
+		manager.Load(behaviour);
+
+		behaviour.foo = "changed-value";
+
+		bool logoutApiCalled = false;
+
+		api.logoutResult = new ServerApi.LogoutResult {
+			type = ServerApi.LogoutResultType.OK
+		};
+
+		api.onLogout = (accessToken, playerData) => {
+			logoutApiCalled = true;
+			Assert.AreEqual("<access-token>", accessToken);
+			Assert.AreEqual(DataRepositoryHelper.ToJsonObject(repo).ToString(), playerData.ToString());
+		};
 
 		manager.Logout();
 
-		o = JsonReader.Parse(
-			PlayerPrefs.GetString(CloudManager.LOCAL_DEBUG_PLAYER_PREFS_KEY, "{}"
-		)).AsJsonObject;
-		Assert.AreEqual("changed again", (string)o["foo"]);
-	}*/
+		Assert.True(logoutApiCalled);
+	}
+
+	[Test]
+	public void LocalDebugPlayerCanBeLoggedOut()
+	{
+		PlayerPrefs.DeleteKey(CloudManager.LocalDebugPlayerPrefsKey);
+		PlayerPrefs.Save();
+
+		manager.Login(null, "local", "");
+		manager.Load(behaviour);
+
+		behaviour.foo = "new-value";
+
+		api.onLogout = (_, __) => Assert.Fail("Logout api method was called when shouldn't.");
+
+		manager.Logout();
+
+		var json = LightJson.Serialization.JsonReader.Parse(
+			PlayerPrefs.GetString(CloudManager.LocalDebugPlayerPrefsKey, "{}")
+		);
+
+		Assert.AreEqual("new-value", (string)json["foo"]);
+		Assert.AreEqual(1, json.AsJsonObject.Count);
+	}
 }
