b956dc58b087b5fe7b024f3651bb35ce011cba46
Jirka-Mayer <jirkamayer97@gmail.com>
2019-10-09 16:35:41 +0200

    v0.7.0

--- Assets/Unisave/NewScripts/AssetMeta.cs
+++ Assets/Unisave/NewScripts/AssetMeta.cs
@@ -8,6 +8,6 @@ namespace Unisave
         /// <summary>
         /// Version of the asset
         /// </summary>
-        public const string Version = "0.7.0-pre-1";
+        public const string Version = "0.7.0";
     }
 }
