f8957b5d6c43697af36c1bd8e7f4fef5a8f1add5
Jirka-Mayer <jirkamayer97@gmail.com>
2020-03-19 20:47:38 +0100

    Fixed up export

--- prepare-export.sh
+++ prepare-export.sh
@@ -8,9 +8,6 @@ cd ./AssetForExport
 rm ./Assets/Unisave/Resources/UnisavePreferencesFile.asset*
 rm -r ./Assets/Unisave/Tests*
 
-# remove chess example (not finished yet)
-rm -r ./Assets/Unisave/Examples/Chess*
-
 # remove example tests
 rm -r ./Assets/Unisave/Examples/Tests*
 
