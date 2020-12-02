8a3e65522f2f7635c2ad6d0c0d1e73f950016c0b
Jirka-Mayer <jirkamayer97@gmail.com>
2020-12-02 14:30:54 +0100

    SessionId is forgotten during restart in a build

--- Assets/Unisave/Scripts/Sessions/ClientSessionIdRepository.cs
+++ Assets/Unisave/Scripts/Sessions/ClientSessionIdRepository.cs
@@ -11,6 +11,18 @@ namespace Unisave.Sessions
     /// </summary>
     public class ClientSessionIdRepository
     {
+        /*
+         * PlayerPrefs?
+         * ------------
+         * 
+         * The unity editor needs to preserve the session id in between
+         * play mode restarts. This is how it does that.
+         * However the build shouldn't do that! It would cause:
+         * - no logout on game close
+         * - two instances of the same app on one PC sharing one session id
+         *     -> problems with broadcasting connection, etc...
+         */
+        
         /// <summary>
         /// In how many minutes the session id expires
         /// </summary>
@@ -27,42 +39,12 @@ namespace Unisave.Sessions
         public string GetSessionId()
         {
             if (!loaded)
-                LoadSessionId();
+            {
+                id = LoadFromPlayerPrefs();
+                loaded = true;
+            }
 
             return id;
-        }
-
-        private void LoadSessionId()
-        {
-            var raw = PlayerPrefs.GetString(PlayerPrefsKey);
-            JsonObject json = null;
-
-            if (string.IsNullOrWhiteSpace(raw))
-                raw = "{}";
-
-            try
-            {
-                json = JsonReader.Parse(raw);
-            }
-            catch (JsonParseException e)
-            {
-                Debug.LogException(e);
-            }
-            
-            if (json == null)
-                json = new JsonObject();
-            
-            // check expiry
-            var storedAt = Serializer.FromJson<DateTime>(json["StoredAt"]);
-            if ((DateTime.UtcNow - storedAt).TotalMinutes > ExpiryMinutes)
-            {
-                id = null;
-                loaded = true;
-                return;
-            }
-
-            id = json["SessionId"].AsString;
-            loaded = true;
         }
         
         /// <summary>
@@ -70,15 +52,58 @@ namespace Unisave.Sessions
         /// </summary>
         public void StoreSessionId(string sessionId)
         {
-            PlayerPrefs.SetString(PlayerPrefsKey, new JsonObject()
-                .Add("StoredAt", Serializer.ToJson(DateTime.UtcNow))
-                .Add("SessionId", sessionId)
-                .ToString()
-            );
-            PlayerPrefs.Save();
-
             id = sessionId;
             loaded = true;
+            
+            RememberInPlayerPrefs();
+        }
+
+        private void RememberInPlayerPrefs()
+        {
+            #if UNITY_EDITOR
+                PlayerPrefs.SetString(PlayerPrefsKey, new JsonObject()
+                    .Add("StoredAt", Serializer.ToJson(DateTime.UtcNow))
+                    .Add("SessionId", id)
+                    .ToString()
+                );
+                PlayerPrefs.Save();
+            #endif
+        }
+
+        private string LoadFromPlayerPrefs()
+        {
+            #if UNITY_EDITOR
+            
+                var raw = PlayerPrefs.GetString(PlayerPrefsKey);
+                JsonObject json = null;
+
+                if (string.IsNullOrWhiteSpace(raw))
+                    raw = "{}";
+
+                try
+                {
+                    json = JsonReader.Parse(raw);
+                }
+                catch (JsonParseException e)
+                {
+                    Debug.LogException(e);
+                }
+                
+                if (json == null)
+                    json = new JsonObject();
+                
+                // check expiry
+                var storedAt = Serializer.FromJson<DateTime>(json["StoredAt"]);
+                if ((DateTime.UtcNow - storedAt).TotalMinutes > ExpiryMinutes)
+                    return null;
+
+                return json["SessionId"].AsString;
+            
+            #else
+
+                return null;
+            
+            #endif
         }
     }
 }
