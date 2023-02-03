0de384c012dfd49732287b84200f823dfd872694
Jirka-Mayer <jirkamayer97@gmail.com>
2023-02-03 10:50:50 +0100

    Finalized home tab

--- Assets/Unisave/Editor/Windows/Main/Tabs/HomeTabController.cs
+++ Assets/Unisave/Editor/Windows/Main/Tabs/HomeTabController.cs
@@ -1,11 +1,18 @@
-using UnityEngine;
+using Unisave.Foundation;
+using UnityEditor.SceneManagement;
 using UnityEngine.UIElements;
+using Application = UnityEngine.Application;
 
 namespace Unisave.Editor.Windows.Main.Tabs
 {
     public class HomeTabController : ITabContentController
     {
         private readonly VisualElement root;
+
+        private VisualElement checklistItemRegister;
+        private VisualElement checklistItemConnect;
+
+        private Label versionsLabel;
 
         public HomeTabController(VisualElement root)
         {
@@ -14,6 +21,8 @@ namespace Unisave.Editor.Windows.Main.Tabs
 
         public void OnCreateGUI()
         {
+            // === Quick link buttons ===
+            
             root.Q<Button>(name: "link-guides").clicked += () => {
                 Application.OpenURL("https://unisave.cloud/guides");
             };
@@ -26,11 +35,68 @@ namespace Unisave.Editor.Windows.Main.Tabs
             root.Q<Button>(name: "link-pricing").clicked += () => {
                 Application.OpenURL("https://unisave.cloud/#pricing");
             };
+            
+            // === Checklist items ===
+
+            checklistItemRegister = root.Q(name: "ci-register");
+            checklistItemConnect = root.Q(name: "ci-connect");
+            
+            // === Checklist buttons ===
+            
+            root.Q<Button>(name: "cb-register").clicked += () => {
+                Application.OpenURL("https://unisave.cloud/register");
+            };
+            root.Q<Button>(name: "cb-connection-tab").clicked += () => {
+                UnisaveMainWindow.ShowTab(MainWindowTab.Connection);
+            };
+            root.Q<Button>(name: "cb-ex-chat").clicked += () => {
+                EditorSceneManager.OpenScene(
+                    "Assets/Unisave/Examples/InBeta/Chat/Chat.unity"
+                );
+            };
+            root.Q<Button>(name: "cb-ex-email-auth").clicked += () => {
+                EditorSceneManager.OpenScene(
+                    "Assets/Unisave/Examples/PlayerAuthentication/PlayerAuthentication.unity"
+                );
+            };
+            root.Q<Button>(name: "cb-storing-player-data").clicked += () => {
+                Application.OpenURL("https://unisave.cloud/guides/how-to-store-player-data-online-with-unity");
+            };
+            root.Q<Button>(name: "cb-sending-emails").clicked += () => {
+                Application.OpenURL("https://unisave.cloud/docs/mail");
+            };
+            root.Q<Button>(name: "cb-steam-microtransactions").clicked += () => {
+                Application.OpenURL("https://unisave.cloud/docs/steam-microtransactions");
+            };
+            
+            root.Q<Button>(name: "cb-discord").clicked += () => {
+                Application.OpenURL("https://discord.gg/XV696Tp");
+            };
+            root.Q<Button>(name: "cb-email").clicked += () => {
+                Application.OpenURL("mailto:info@unisave.cloud");
+            };
+            
+            // === Other ===
+
+            versionsLabel = root.Q<Label>(name: "versions-label");
         }
 
         public void OnObserveExternalState()
         {
-            // nothing
+            var preferences = UnisavePreferences.LoadOrCreate();
+            
+            bool notConnected = string.IsNullOrWhiteSpace(preferences.GameToken) ||
+                string.IsNullOrWhiteSpace(preferences.EditorKey);
+            
+            checklistItemRegister.EnableInClassList(
+                "checklist-item--hidden", enable: !notConnected
+            );
+            checklistItemConnect.EnableInClassList(
+                "checklist-item--hidden", enable: !notConnected
+            );
+
+            versionsLabel.text = $"Unisave Asset: {AssetMeta.Version}\n" +
+                                 $"Unisave Framework: {FrameworkMeta.Version}";
         }
 
         public void OnWriteExternalState()
