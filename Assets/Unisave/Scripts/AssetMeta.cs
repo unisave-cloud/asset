85873b490cef97bfee41720014a4f7ba3de3a66c
Jirka-Mayer <jirkamayer97@gmail.com>
2023-02-13 20:49:25 +0100

    v0.11.0-rc.1

--- Assets/Unisave/Scripts/AssetMeta.cs
+++ Assets/Unisave/Scripts/AssetMeta.cs
@@ -8,6 +8,6 @@ namespace Unisave
         /// <summary>
         /// Version of the asset
         /// </summary>
-        public static readonly string Version = "0.11.0-dev";
+        public static readonly string Version = "0.11.0-rc.1";
     }
 }
