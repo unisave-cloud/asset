a57bb6491dcd460be785b5d956ef161d06423580
Jirka-Mayer <jirkamayer97@gmail.com>
2023-02-13 21:08:54 +0100

    Updated the exporting script

--- scripts/prepare-export.sh
+++ scripts/prepare-export.sh
@@ -4,11 +4,11 @@
 # This script prepares the repository for export to Unity Asset Store.
 # It does that by making a copy of this directory and preparing it.
 
-# copy the Asset directory to AssetForExport and go into the copy
-cd ../../
-rm -rf ./AssetForExport
-cp -r ./Asset ./AssetForExport
-cd ./AssetForExport
+# copy the asset directory to asset-for-export and go into the copy
+cd ../
+rm -rf ./asset-for-export
+cp -r ./asset ./asset-for-export
+cd ./asset-for-export
 
 # remove unnecessary unisave files
 rm ./Assets/Unisave/Resources/UnisavePreferencesFile.asset*
