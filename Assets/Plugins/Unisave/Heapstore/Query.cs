34c164e3872ef5f15e7e0667c3b13b17e5e588cf
Jirka-Mayer <jirkamayer97@gmail.com>
2023-06-02 21:38:21 +0200

    Defined query tests

--- Assets/Plugins/Unisave/Heapstore/Query.cs
+++ Assets/Plugins/Unisave/Heapstore/Query.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -32,15 +33,85 @@ namespace Unisave.Heapstore
         {
             return this;
         }
+
+        public Query Sort(string field, string direction)
+        {
+            return Sort(
+                (field, direction)
+            );
+        }
+        
+        public Query Sort(params ValueTuple<string, string>[] fieldsAndDirections)
+        {
+            return this;
+        }
+
+        public Query Limit(int take)
+        {
+            return Limit(skip: 0, take: take);
+        }
+
+        public Query Limit(int skip, int take)
+        {
+            return this;
+        }
         
         
         ///////////////
         // Execution //
         ///////////////
         
+        /// <summary>
+        /// Execute the query and get all the matching documents
+        /// </summary>
         public UnisaveOperation<List<Document>> Get()
         {
             return new UnisaveOperation<List<Document>>(caller, GetAsync());
+        }
+
+        /// <summary>
+        /// Execute the query and get all the documents
+        /// converted to the given type
+        /// </summary>
+        /// <typeparam name="T">The type to convert the documents to</typeparam>
+        public UnisaveOperation<List<T>> GetAs<T>()
+        {
+            return new UnisaveOperation<List<T>>(caller, async () => {
+                List<Document> documents = await GetAsync();
+                return documents.Select(d => d.As<T>()).ToList();
+            });
+        }
+
+        /// <summary>
+        /// Execute the query and return the first document to match,
+        /// or null if no documents match
+        /// </summary>
+        public UnisaveOperation<Document> First()
+        {
+            // TODO: add a Limit(1) clause (or rather, set the "take", keep "skip")
+            
+            return new UnisaveOperation<Document>(caller, async () => {
+                List<Document> documents = await GetAsync();
+                if (documents.Count == 0)
+                    return null;
+                return documents[0];
+            });
+        }
+
+        /// <summary>
+        /// Execute the query and return the first document to match,
+        /// or null if no documents match. Then convert the document
+        /// to the given type.
+        /// </summary>
+        /// <typeparam name="T">The type to convert the document to</typeparam>
+        public UnisaveOperation<T> FirstAs<T>()
+        {
+            return new UnisaveOperation<T>(caller, async () => {
+                Document document = await First();
+                if (document == null)
+                    return default(T);
+                return document.As<T>();
+            });
         }
 
         private async Task<List<Document>> GetAsync()
--- Assets/UnisaveFixture/Tests/Heapstore/QueryApiTest.cs
+++ Assets/UnisaveFixture/Tests/Heapstore/QueryApiTest.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using LightJson;
 using NUnit.Framework;
+using Unisave;
 using Unisave.Facets;
 using Unisave.Heapstore;
 using Unisave.Serialization;
@@ -28,6 +30,15 @@ namespace UnisaveFixture.Tests.Heapstore
                 f.ClearDatabase()
             );
         });
+        
+        private class Item
+        {
+            [SerializeAs("_id")]
+            public string id;
+
+            public string name;
+            public int price;
+        }
 
         private async Task GenerateItems(int count)
         {
@@ -89,6 +100,233 @@ namespace UnisaveFixture.Tests.Heapstore
                 .Get();
         
             Assert.AreEqual(0, documents.Count);
+        });
+        
+        [UnityTest]
+        public IEnumerator FirstReturnsTheFirstDocument()
+            => Asyncize.UnityTest(async () =>
+        {
+            await GenerateItems(count: 1);
+
+            Document document = await caller
+                .Collection("items")
+                .First();
+        
+            Assert.AreEqual(1, document.Data["price"].AsInteger);
+        });
+        
+        [UnityTest]
+        public IEnumerator FirstReturnsNullWhenNoDocuments()
+            => Asyncize.UnityTest(async () =>
+        {
+            Document document = await caller
+                .Collection("items")
+                .First();
+    
+            Assert.IsNull(document);
+        });
+        
+        [UnityTest]
+        public IEnumerator FirstConvertsToCustomType()
+            => Asyncize.UnityTest(async () =>
+        {
+            await GenerateItems(count: 1);
+            
+            Item item = await caller
+                .Collection("items")
+                .FirstAs<Item>();
+
+            Assert.AreEqual("item_1", item.name);
+            Assert.AreEqual(1, item.price);
+        });
+        
+        [UnityTest]
+        public IEnumerator FirstConvertsToNullWhenNoDocuments()
+            => Asyncize.UnityTest(async () =>
+        {
+            Item item = await caller
+                .Collection("items")
+                .FirstAs<Item>();
+
+            Assert.IsNull(item);
+        });
+        
+        [UnityTest]
+        public IEnumerator GetConvertsToCustomType()
+            => Asyncize.UnityTest(async () =>
+        {
+            await GenerateItems(count: 10);
+            
+            List<Item> items = await caller
+                .Collection("items")
+                .GetAs<Item>();
+
+            items.Sort((a, b) => a.price.CompareTo(b.price));
+            
+            Assert.AreEqual(10, items.Count);
+            for (int i = 1; i <= 10; i++)
+            {
+                Assert.AreEqual("item_" + i, items[i-1].name);
+                Assert.AreEqual(i, items[i-1].price);
+            }
+        });
+        
+        
+        ///////////////////
+        // Filter clause //
+        ///////////////////
+        
+        [UnityTest]
+        public IEnumerator FilterSelectsByEquality()
+            => Asyncize.UnityTest(async () =>
+        {
+            await GenerateItems(count: 10);
+            
+            List<Document> documents = await caller
+                .Collection("items")
+                .Filter("price", "==", 5)
+                .Get();
+    
+            Assert.AreEqual(1, documents.Count);
+            Assert.AreEqual("item_5", documents[0].Data["name"].AsString);
+        });
+        
+        [UnityTest]
+        public IEnumerator FilterByMultipleClauses()
+            => Asyncize.UnityTest(async () =>
+        {
+            await GenerateItems(count: 10);
+        
+            List<Item> items = await caller
+                .Collection("items")
+                .Filter("price", ">=", 2)
+                .Filter("price", "<=", 5)
+                .GetAs<Item>();
+
+            items.Sort((a, b) => a.price.CompareTo(b.price));
+            
+            Assert.AreEqual(4, items.Count);
+            for (int i = 2; i <= 5; i++)
+            {
+                Assert.AreEqual("item_" + i, items[i-2].name);
+                Assert.AreEqual(i, items[i-2].price);
+            }
+        });
+        
+        
+        /////////////////
+        // Sort clause //
+        /////////////////
+        
+        [UnityTest]
+        public IEnumerator SortAscending()
+            => Asyncize.UnityTest(async () =>
+        {
+            await GenerateItems(count: 10);
+    
+            List<Item> items = await caller
+                .Collection("items")
+                .Sort("price", "ASC")
+                .GetAs<Item>();
+
+            Assert.AreEqual(10, items.Count);
+            for (int i = 1; i <= 10; i++)
+                Assert.AreEqual(i, items[i-1].price);
+        });
+        
+        [UnityTest]
+        public IEnumerator SortDescending()
+            => Asyncize.UnityTest(async () =>
+        {
+            await GenerateItems(count: 10);
+
+            List<Item> items = await caller
+                .Collection("items")
+                .Sort("price", "DESC")
+                .GetAs<Item>();
+
+            Assert.AreEqual(10, items.Count);
+            for (int i = 1; i <= 10; i++)
+                Assert.AreEqual(11 - i, items[i-1].price);
+        });
+        
+        [UnityTest]
+        public IEnumerator SortMultiple()
+            => Asyncize.UnityTest(async () =>
+        {
+            await caller.CallFacet(
+                (RawAqlFacet f) => f.CreateCollection("items")
+            );
+            await caller.CallFacet((RawAqlFacet f) => f.Run(@"
+                FOR doc IN [
+                    { name: 'A', price: 1 },
+                    { name: 'B', price: 1 },
+                    { name: 'C', price: 2 },
+                    { name: 'D', price: 2 }
+                ] INSERT doc INTO items
+            "));
+
+            List<Item> items = await caller
+                .Collection("items")
+                .Sort(("price", "DESC"), ("name", "ASC"))
+                .GetAs<Item>();
+
+            string result = string.Concat(items.Select(i => i.name));
+            Assert.AreEqual("CDAB", result);
+        });
+        
+        
+        //////////////////
+        // Limit clause //
+        //////////////////
+        
+        [UnityTest]
+        public IEnumerator LimitTake()
+            => Asyncize.UnityTest(async () =>
+        {
+            await GenerateItems(count: 10);
+
+            List<Item> items = await caller
+                .Collection("items")
+                .Sort("price", "ASC")
+                .Limit(take: 3)
+                .GetAs<Item>();
+
+            Assert.AreEqual(3, items.Count);
+            for (int i = 1; i <= 3; i++)
+                Assert.AreEqual(i, items[i-1].price);
+        });
+        
+        [UnityTest]
+        public IEnumerator LimitSkipTake()
+            => Asyncize.UnityTest(async () =>
+        {
+            await GenerateItems(count: 10);
+
+            List<Item> items = await caller
+                .Collection("items")
+                .Sort("price", "ASC")
+                .Limit(skip: 2, take: 3)
+                .GetAs<Item>();
+
+            Assert.AreEqual(3, items.Count);
+            for (int i = 1; i <= 3; i++)
+                Assert.AreEqual(i + 2, items[i-1].price);
+        });
+        
+        [UnityTest]
+        public IEnumerator LimitSkipTakeFirst()
+            => Asyncize.UnityTest(async () =>
+        {
+            await GenerateItems(count: 10);
+
+            Item item = await caller
+                .Collection("items")
+                .Sort("price", "ASC")
+                .Limit(skip: 2, take: 6)
+                .FirstAs<Item>();
+
+            Assert.AreEqual(3, item.price);
         });
     }
 }
