f64a30ce89ba2c88db6b8532c4895bcd45f2d3c3
Jiří Mayer <jirkamayer97@gmail.com>
2023-12-29 11:52:21 +0100

    Cleaned up export script

--- scripts/prepare-export.sh
+++ scripts/prepare-export.sh
@@ -10,12 +10,5 @@ rm -rf ./asset-for-export
 cp -r ./asset ./asset-for-export
 cd ./asset-for-export
 
-# remove unnecessary unisave files
-rm ./Assets/Plugins/Unisave/Resources/UnisavePreferencesFile.asset*
-
 # remove unisave fixture
 rm -r ./Assets/UnisaveFixture*
-
-# disable Heapstore backend
-HEAPSTORE_BACKEND_FILE=./Assets/Plugins/Unisave/Heapstore/Backend/HeapstoreBackend.asset
-sed -i "s/uploadBehaviour: always/uploadBehaviour: never/" $HEAPSTORE_BACKEND_FILE
